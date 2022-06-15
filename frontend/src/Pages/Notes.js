98d86d95de2717de582d9028a73c49d48a4ac87c
stevekindstrand <stevekindstrand@gmail.com>
2022-06-15 11:18:43 +0200

    added color picker

--- frontend/src/Pages/Notes.js
+++ frontend/src/Pages/Notes.js
@@ -66,7 +66,22 @@ const Notes = () => {
         <Editor
           onInit={(e, editor) => (editorRef.current = editor)}
           initialValue="<p>New text</p>"
-          init={{ menubar: false }}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist autolink lists link image charmap print preview anchor",
+              "searchreplace visualblocks code fullscreen",
+              "insertdatetime media table paste code help wordcount",
+            ],
+            toolbar:
+              "undo redo | formatselect | " +
+              "bold italic | backcolor forecolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent | " +
+              "removeformat | help",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
         />
         <div>
           <input
